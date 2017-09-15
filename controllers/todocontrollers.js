6cae42f85b938c5ebbf846edb39798479dd7dbed
Varun Rajvanshi <varunrajvanshi@gmail.com>
2017-09-15 23:31:44 +0530

    Update todocontrollers.js

--- controllers/todocontrollers.js
+++ controllers/todocontrollers.js
@@ -1,29 +1,25 @@
 var bodyParser=require('body-parser');
+var data=[{item:'get milk'},{item:'buy ketchup'},{item:'learn mongo'}];
+var urlencodedParser = bodyParser.urlencoded({extended:false});
 
-var data=[{item:'item1'},{item:'buy ketchup'},{item:'item3'}];
+module.exports = function(app){
 
-var urlencodedParser=bodyParser.urlencoded({extended:false});
-module.exports=function(app){
-
-
-  app.get('/todo',function(req,res)
-  {
- 	console.log("Inside get");
+  app.get('/todo',function(req,res){
     res.render('todo',{todos:data});
   });
 
   app.post('/todo',urlencodedParser,function(req,res){
-	console.log("inside post adding ");
-	data.push(req.body);
-	res.json(data);
+    data.push(req.body);
+    res.json(data);
   });
 
   app.delete('/todo/:item',function(req,res){
-    data=data.filter(function(data){
-	console.log("inside delete");
-      return data.item.replace(/ /g,'-') !== req.params.items;
-  });
+    data=data.filter(function(todo){
+      return todo.item.replace(/ /g,'-') !== req.params.item;
+    });
+    res.json(data);
   });
 
 
-}
+};
+
